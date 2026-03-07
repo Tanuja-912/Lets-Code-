7e41ea30d88b57a1001dc9faaccb44fffa8bf783
Tanuja-912 <kondapaturitanuja@gmail.com>
2026-03-07 18:30:00 +0530

    Add reverse integer method in Solution class
    Implement digit-by-digit reversal of an integer.

--- Basic_math.java
+++ Basic_math.java
@@ -1,1 +1,20 @@
+//Day1-->Basic Math in java to have an idea on fundamentals
+//Lets Reverse a number
+//Leetcode Problem Link: https://leetcode.com/problems/reverse-integer/
+//Approach: Digit by Digit Reversal (Iterative)
 
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while(x != 0){
+            int d = x % 10;
+            x = x / 10;
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rev = rev * 10 + d;
+        }
+        return rev;
+    }
+}
